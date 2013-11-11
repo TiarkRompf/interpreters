2519d0f9c68da9759f8119f2d7e0da7f05700e01
Jacques Carette <carette@mcmaster.ca>
2013-11-11 16:50:51 -0500

    split off Domains

--- Haskell/testAbsInt.hs
+++ Haskell/testAbsInt.hs
@@ -18,50 +18,7 @@ import Data.Bits ((.&.))
 import Data.Char (ord)
 
 import Lattice
-
--- an class with no methods, just to allow us to declare domains
-class Domain d where
-instance Domain Int
-instance Domain Bool
-
---    Sign
-data Sign = Neg | Zero | Pos deriving (Eq, Show)
-instance Domain Sign
-
--- this is partial, but won't be used raw
-splus :: Sign -> Sign -> Sign
-splus Pos Pos = Pos
-splus Neg Neg = Neg
-splus Zero x  = x
-splus x Zero  = x
-
--- NSign, a more refined lattice
-data NSign = NSPos | NSNeg | NSZero | NSZNeg | NSZPos 
-    deriving (Eq, Show)
--- not a Domain
-
--- partial
-nsplus :: NSign -> NSign -> NSign
-nsplus NSPos  NSPos  = NSPos
-nsplus NSNeg  NSNeg  = NSNeg
-nsplus NSZero x      = x
-nsplus x      NSZero = x
-nsplus NSNeg  NSZNeg = NSNeg
-nsplus NSPos  NSZPos = NSPos
-nsplus NSZNeg NSZNeg = NSZNeg
-nsplus NSZNeg NSNeg  = NSNeg
-nsplus NSZPos NSPos  = NSPos
-nsplus NSZPos NSZPos = NSZPos
-
--- Parity
-data Parity = Even | Odd deriving (Eq, Show)
-instance Domain Parity
-
-pplus :: Parity -> Parity -> Parity
-pplus Even Even = Even
-pplus Odd  Odd  = Even
-pplus Odd  Even = Odd
-pplus Even Odd  = Odd
+import Domains
 
 -- For 'Abstract' interpretation 
 -- b is an abstraction of values of a
