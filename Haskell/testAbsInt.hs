162e70e27197c1266406d48eb5468d6023bc597f
Jacques Carette <carette@mcmaster.ca>
2013-11-11 22:28:38 -0500

    more simplifications of the types

--- Haskell/testAbsInt.hs
+++ Haskell/testAbsInt.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, NoMonomorphismRestriction, 
              FlexibleInstances, KindSignatures, StandaloneDeriving,
-             FlexibleContexts, UndecidableInstances
+             FlexibleContexts, UndecidableInstances, TypeFamilies,
+             ConstraintKinds
              #-}
 module Main where
 
@@ -14,23 +15,29 @@ import Control.Monad.Identity
 import qualified Control.Monad.Trans.State as S
 import Data.Maybe (fromJust)
 import Data.Char (ord)
+import GHC.Exts
 
 import Lattice
 import Domains
 import Abstract
 
 -- The core components under our language, in tagless style
-class SymExpr prep b where
-  int_ :: Int -> prep b
-  plus_ :: prep b -> prep b -> prep b
-  times_ :: prep b -> prep b -> prep b
+-- val ~ value representation
+class SymExpr val b where
+  int_ :: Int -> val b
+  plus_ :: val b -> val b -> val b
+  times_ :: val b -> val b -> val b
 
+-- vrep ~ variable representation
 class SymVar vrep a where
   s_ :: String -> vrep a
 
-class SymControl c b where
+class SymNZControl c b where
   ifNonZero :: c b -> c () -> c () -> c ()
   whileNonZero :: c b -> c () -> c ()
+
+class Runnable c h where
+  run :: c h () -> h
 
 instance (Abstract Int b, Additive b, Multiplicative b) => SymExpr FlatLattice b where
   int_ x = Embed $ abstract x
@@ -45,17 +52,15 @@ instance (Abstract Int b, Additive b, Multiplicative b) => SymExpr GLattice b wh
 class Empty s where
   empty :: s
 
-class Empty (h p v a) => Heap h p v a where
-  insert :: v a -> p a -> h p v a -> h p v a
-  lookup :: v a -> h p v a -> Maybe (p a)
+class Heap h where
+  type HeapCtx h :: * -> Constraint
+  insert :: HeapCtx h (v a) => v a -> p a -> h p v a -> h p v a
+  lookup :: HeapCtx h (v a) => v a -> h p v a -> Maybe (p a)
 
 class Mutation m h p v a where
   newRef   :: String -> p a -> m (h p v a) (v a)
   readRef  :: v a -> m (h p v a) (p a)
   writeRef :: v a -> p a -> m (h p v a) ()
-
-class Runnable c h where
-  run :: c h () -> h
 
 newtype ST s a = ST (S.State s a)
 unST (ST x) = x
@@ -64,14 +69,14 @@ instance Monad (ST s) where
   return = ST . return
   (ST m) >>= k = ST $ m >>= unST . k
 
-instance (Heap h p v a, SymVar v a) => Mutation ST h p v a where
+instance (Heap h, HeapCtx h (v a), SymVar v a) => Mutation ST h p v a where
   newRef nm l = ST $ do S.modify $ insert (s_ nm) l
                         return $ s_ nm
   readRef vr = ST $ fromJust . lookup vr <$> S.get
   writeRef vr l = ST $ S.modify $ insert vr l
 
 instance (LowerLattice b, Eq b, Abstract Int b, Eq s, Empty s, LowerLattice s) => 
-    SymControl (ST s) b where
+    SymNZControl (ST s) b where
   ifNonZero cond statTrue statFalse = ST $
     do c <- unST cond
        s <- S.get
@@ -85,11 +90,11 @@ instance (LowerLattice b, Eq b, Abstract Int b, Eq s, Empty s, LowerLattice s) =
        st1 <- S.get
        unless (st1 == st0) (unST $ whileNonZero cond body)
 
-instance (Heap h p v a) => Runnable ST (h p v a) where
+instance (Heap h, Empty (h p v a)) => Runnable ST (h p v a) where
   run (ST body) = S.execState body empty
     
 -- The explicit ST below forces a number of the instances above to be chosen
-program :: Heap h p v a => ST (h p v a) () -> h p v a
+program :: (Heap h, Empty (h p v a)) => ST (h p v a) () -> h p v a
 program body = run body
        
 -- An actual instance to test with
@@ -117,7 +122,8 @@ instance (Show (p a), Show (v a)) => Show (MapHeap p v a) where
 instance Empty (MapHeap p v a) where
   empty = H $ Map.empty
 
-instance Ord (v a) => Heap MapHeap p v a where
+instance Heap MapHeap where
+  type HeapCtx MapHeap = Ord
   insert v r h = H $ Map.insert v r (unH h)
   lookup v h = Map.lookup v (unH h)
 
@@ -128,8 +134,7 @@ instance (Eq a, Ord (v a), LowerLattice (p a)) => LowerLattice (MapHeap p v a) w
   lub s1 s2 = H $ Map.foldrWithKey (Map.insertWith lub) (unH s1) (unH s2)
 
 -- These combinators are even more useful to make 'real' programs 
-int :: (Heap h p v a, SymExpr p b, Abstract Int b) => 
-       Int -> ST (h p v a) (p b)
+int :: (Monad m, SymExpr p b) => Int -> m (p b)
 int = return . int_
 
 plus = liftM2 plus_
@@ -137,20 +142,21 @@ newVar nm vl = newRef nm =<< vl
 readVar v = readRef v
 writeVar nm vl = writeRef nm =<< vl
 
--- type signatures needed because programs are so polymorphic!
-prog1 :: (Eq b, Abstract Int b, Additive b,
-  SymVar v b, Ord (v b), LowerLattice (p b),
-  Heap h p v b, LowerLattice (h p v b), Eq (h p v b),
-  Abstract Int (p b), Eq (p b), SymExpr p b) => h p v b
-prog1 = program (
+-- type signatures needed
+pprog1 :: (SymExpr p a, Monad (m (h p v a)), Mutation m h p v a, 
+   SymNZControl (m (h p v a)) (p a)) => m (h p v a) ()
+pprog1 = 
   do 
     c <- newVar "c" (int 0)
     whileNonZero (readVar c) (
-      writeVar c (plus (readVar c) (int 1)) ) )
-  
+      writeVar c (plus (readVar c) (int 1)) )
+   
+prog1 = program pprog1
+
 prog2 :: (Eq b, Abstract Int b, Additive b,
   SymVar v b, Ord (v b), LowerLattice (p b),
-  Heap h p v b, LowerLattice (h p v b), Eq (h p v b),
+  Heap h, LowerLattice (h p v b), Eq (h p v b),
+  Empty (h p v b), HeapCtx h (v b),
   Abstract Int (p b), Eq (p b), SymExpr p b) => h p v b
 prog2 = program (
   do 
@@ -160,12 +166,13 @@ prog2 = program (
   where
     -- the signature is needed because Haskell correctly infers that 
     -- this constant can live somewhere else than the global ones
-    const1 :: (Abstract Int b0, SymExpr p0 b0, Heap h0 p0 v0 b0) => ST (h0 p0 v0 b0) (p0 b0)
+    const1 :: (Abstract Int b0, SymExpr p0 b0, Heap h0) => ST (h0 p0 v0 b0) (p0 b0)
     const1 = int 1
   
 prog3 :: (Abstract Int b,
   SymVar v b, Ord (v b),
-  Heap h p v b, LowerLattice (h p v b), Eq (h p v b),
+  Heap h, LowerLattice (h p v b), Eq (h p v b),
+  Empty (h p v b), HeapCtx h (v b),
   Abstract Int (p b), LowerLattice (p b), Eq (p b), SymExpr p b) => 
   Int -> h p v b
 prog3 = \n -> program (
