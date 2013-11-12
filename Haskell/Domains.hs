7053e1293380f4bbe79c7a3f20d1ad11cfdd50cf
Jacques Carette <carette@mcmaster.ca>
2013-11-11 19:48:26 -0500

    implement multiplication

--- Haskell/Domains.hs
+++ Haskell/Domains.hs
@@ -19,6 +19,14 @@ splus Neg Neg = Neg
 splus Zero x  = x
 splus x Zero  = x
 
+stimes :: Sign -> Sign -> Sign
+stimes Pos Pos = Pos
+stimes Neg Neg = Pos
+stimes Zero _  = Zero
+stimes _ Zero  = Zero
+stimes Pos Neg = Neg
+stimes Neg Pos = Neg
+
 -- NSign, a more refined lattice
 data NSign = NSPos | NSNeg | NSZero | NSZNeg | NSZPos 
     deriving (Eq, Show)
@@ -37,6 +45,26 @@ nsplus NSZNeg NSNeg  = NSNeg
 nsplus NSZPos NSPos  = NSPos
 nsplus NSZPos NSZPos = NSZPos
 
+nstimes :: NSign -> NSign -> NSign
+nstimes NSPos  NSPos  = NSPos
+nstimes NSNeg  NSNeg  = NSPos
+nstimes NSNeg  NSPos  = NSNeg
+nstimes NSPos  NSNeg  = NSNeg
+nstimes NSPos  NSZNeg = NSZNeg
+nstimes NSZero _      = NSZero
+nstimes _      NSZero = NSZero
+nstimes NSNeg  NSZNeg = NSZPos
+nstimes NSNeg  NSZPos = NSZNeg
+nstimes NSPos  NSZPos = NSZPos
+nstimes NSZNeg NSZNeg = NSZPos
+nstimes NSZNeg NSNeg  = NSZPos
+nstimes NSZNeg NSPos  = NSZNeg
+nstimes NSZNeg NSZPos = NSZNeg
+nstimes NSZPos NSPos  = NSZPos
+nstimes NSZPos NSNeg  = NSZNeg
+nstimes NSZPos NSZNeg = NSZNeg
+nstimes NSZPos NSZPos = NSZPos
+
 -- Parity
 data Parity = Even | Odd deriving (Eq, Show)
 instance Domain Parity
@@ -46,3 +74,9 @@ pplus Even Even = Even
 pplus Odd  Odd  = Even
 pplus Odd  Even = Odd
 pplus Even Odd  = Odd
+
+ptimes :: Parity -> Parity -> Parity
+ptimes Even Even = Even
+ptimes Odd  Odd  = Odd
+ptimes Odd  Even = Even
+ptimes Even Odd  = Even
